8f645ea3e0104fea2e2fb038ffa87bd6a1c9c464
Jonathan White <jadeofsapphire@pm.me>
2025-12-03 02:23:50 -0700

    Scan

--- src/scan.ts
+++ src/scan.ts
@@ -1,25 +1,55 @@
 import { NS } from "@ns";
+import { Server } from "@ns";
 
-export async function main(ns: NS): Promise<void> {
-  let servers = scan(ns, ns.getHostname());
-  ns.tprint(servers);
+export interface ServerSummary {
+    hostname: string;
+
+    purchasedByPlayer: boolean;
+    hasAdminRights: boolean;
+    backdoorInstalled: boolean;
+
+    requiredHackingSkill: number;
+    moneyMax: number;
+    minDifficulty: number;
+    maxRam: number;
 }
 
-function scan(ns: NS, starting_server: string): string[] {
-  let visited = new Set<string>();
-  const result: string[] = [];
+function collectAllServerData(ns: NS): ServerSummary[] {
+	let known_servers = new Set<string>();
+	let result: ServerSummary[] = [];
 
-  function visit(node: string) {
-    if (visited.has(node))
-      return;
-    visited.add(node);
-    result.push(node);
+	function visit(node: string) {
+		if (known_servers.has(node))
+			return;
+		known_servers.add(node);
 
-    let servers = ns.scan(node);
-    for (let server in servers) {
-      visit(server);
-    }
-  }
-  visit(starting_server);
-  return result;
+		let s = ns.getServer(node);
+
+		result.push({
+			hostname: s.hostname,
+			purchasedByPlayer: s.purchasedByPlayer,
+			hasAdminRights: s.hasAdminRights,
+			backdoorInstalled: s.backdoorInstalled ?? false,
+			requiredHackingSkill: s.requiredHackingSkill ?? 0,
+			moneyMax: s.moneyMax ?? 0,
+			minDifficulty: s.minDifficulty ?? 0,
+			maxRam: s.maxRam,
+		});
+		let neighbors = ns.scan(node);
+		
+		for (let neighbor in neighbors) {
+			if (!known_servers.has(neighbor)) {
+				visit(neighbor);
+			}
+		}
+	}
+	visit("home");
+	return result;
+}
+
+export async function main(ns: NS): Promise<void> {
+  const servers = collectAllServerData(ns);
+  const json = JSON.stringify(servers, null, 2);
+  
+  ns.write("/data/all_servers.json", json, "w");
 }
